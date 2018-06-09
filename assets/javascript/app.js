215904c73754d479f3cfa09aaeff3873b45d5c22
Gina Paik <ginapaik@outlook>
2018-06-09 18:57:47 -0400

    updated questions and gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,47 @@
 var triviaQuestions = [{
-	question: "In what year was Pixar founded?",
-	answerList: ["1979", "1986", "1995", "2000"],
-	answer: 1
-},{
-	question: "Which tech mogul provided funding and became a co-founder of Pixar?",
-	answerList: ["Steve Jobs", "Bill Gates", "Peter Thiel", "Mark Zuckerberg"],
-	answer: 0
-},{
-	question: "What was Pixar's first feature-length film that was released in 1995?",
-	answerList: ["Toy Story", "A Bug's Life", "Monster's Inc", "Finding Nemo"],
-	answer: 0
-},{
-	question: "Who was the first Pixar character added to the Disney Princess line-up?",
-	answerList: ["Jessie", "Repunzel", "Merida", "Elsa"],
+	question: "Which of the below is a foundational CrossFit movement?",
+	answerList: ["Bicep curls", "Calf raises", "Deadlift", "Jumping Jacks"],
 	answer: 2
 },{
-	question: "What's the name of Pixar's first short film, also known as their mascot?",
-	answerList: ["Lampo", "Junior", "Pixie", "Luxo Jr."],
-	answer: 3
-},{
-	question: "How many sequels does Pixar currently have released? (as of August 2016)",
-	answerList: ["5", "3", "6", "7"],
-	answer: 0
-},{
-	question: "Which film won Pixar's first Academy Award for Best Animated Feature?",
-	answerList: ["Toy Story", "Finding Nemo", "Up", "Wall-E"],
+	question: "Who was named the Fittest Man on Earth in 2017?",
+	answerList: ["Brent Fikowski", "Mat Frasier", "Rich Froning", "Ben Smith"],
 	answer: 1
 },{
-	question: "Who directed Pixar's first three feature films?",
-	answerList: ["Peter Docter", "Brad Bird", "John Lasseter", "Peter Sohn"],
+	question: "This WOD requires the athlete to complete 30 snatches for time:",
+	answerList: ["Isabel", "Cindy", "Fran", "Nicole"],
+	answer: 0
+},{
+	question: "Who invented CrossFit?",
+	answerList: ["Samantha Briggs", "Thomas Crubaugh", "Denise Thomas", "Greg Glassman"],
+	answer: 3
+},{
+	question: "Where were the first CrossFit games held?",
+	answerList: ["Los Angeles, CA", "San Diego, CA", "Aromas, CA", "Carson, CA"],
 	answer: 2
 },{
-	question: "Who voiced Sadness in 'Inside Out'?",
-	answerList: ["Amy Poehler", "Phyllis Smith", "Mindy Kaling", "Phyllis Vance"],
+	question: "Who is the offical sponsor of the CrossFit games?",
+	answerList: ["Nike", "Reebok", "Under Armour", "Rogue"],
 	answer: 1
 },{
-	question: "Billy Crystal voices Mike Wazowski in 'Monster, Inc.' but what role did he originally turn down from Pixar?",
-	answerList: ["Hopper", "Woody", "Marlin", "Buzz Lightyear"],
-	answer: 3
-},{
-	question: "The voice of WALL-E, Ben Burtt, also voiced what other famous robot?",
-	answerList: ["R2-D2", "Alpha 5", "C-3PO", "Astro Boy"],
+	question: "CrossFit gyms (or boxes) are referred to this by the CrossFit corporate offices:",
+	answerList: ["Affiliates", "Franchises", "Branches", "Offices"],
 	answer: 0
 },{
-	question: "Brad Bird directed which animated film prior to taking on 'The Incredibles'?",
-	answerList: ["The Brave Little Toaster", "The Iron Giant", "Tarzan", "The Prince of Egypt"],
-	answer: 1
-},{
-	question: "Pixar was originally a division of which studio?",
-	answerList: ["Dreamworks", "Industrial Light & Magic", "Disney", "Lucasfilm"],
+	question: "Which type of pull up involves using momentum to get your chin above the bar?",
+	answerList: ["Fast", "Strict", "Jerking", "Kipping"],
 	answer: 3
 },{
-	question: "What is the name of the famour explorer from 'Up' that Carl looked up to as a boy?",
-	answerList: ["Charles F. Muntz", "Chuck M. James", "Charlie Rose", "Carl Carlton"],
-	answer: 0
-},{
-	question: "'A Bug's Life' was loosely based on what other film?",
-	answerList: ["I Live in Fear", "Rashomon", "Seven Samurai", "Magneficent Seven"],
+	question: "This exercise works primarily your legs:",
+	answerList: ["Snatch", "Toes to bar", "Squat", "Clean"],
 	answer: 2
+},{
+	question: "What does scalability refer to?",
+	answerList: ["Modifying the prescribed workout", "Creating your own workout from scratch", "Running 1 mile prior to the WOD", "Recovery"],
+	answer: 0
 }];
 
-var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13','question14','question15'];
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 
+'question7', 'question8', 'question9', 'question10'];
 var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
 var messages = {
 	correct: "Yes, that's right!",
